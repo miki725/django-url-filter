f5e7bb3a7c04d6ff78ce1779a77dbee2ef016dd6
Bastien Vallet <b.vallet@criteo.com>
2020-05-15 17:14:41 +0200

    Remove python 2.x support

--- setup.py
+++ setup.py
@@ -41,10 +41,10 @@ setup(
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Development Status :: 2 - Pre-Alpha",
     ],
 )
